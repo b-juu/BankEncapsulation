103f638e5cc42640772ddf66f84f756c7a657056
b-juu <erichassett4@gmail.com>
2023-10-09 14:39:09 -0400

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0.0;
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited ${amount}. New balance: ${balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount. Amount must be greater than 0.");
+            }
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace BankEncapsulation
+﻿using BankEncapsulation;
+
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        BankAccount account = new BankAccount();
+
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Get Balance");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choose an option: ");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the amount to deposit: ");
+                        double depositAmount;
+                        if (double.TryParse(Console.ReadLine(), out depositAmount))
+                        {
+                            account.Deposit(depositAmount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input for deposit amount.");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine($"Current balance: ${account.GetBalance()}");
+                        break;
+                    case 3:
+                        Console.WriteLine("Exiting the program.");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please select a valid option.");
+            }
         }
     }
 }
